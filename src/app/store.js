1c66a26c483338dc781177b92243140051475601
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2024-06-13 12:48:59 -0600

    Style: formato

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { taskSlice } from "../features/task/taskSlice";
 
 const store = configureStore({
-    reducer:{task: taskSlice.reducer}
+    reducer: { task: taskSlice.reducer }
 }); /* COMO UN useState externo */
 
-export {store};
+export { store };
